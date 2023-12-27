704c28c6cfaab122c7c0f0f5a05dd7a8b5ecb65b
Kaung Htet <51477974+just-kaunghtet@users.noreply.github.com>
2023-12-27 08:56:54 +0630

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Header() {
     return (
         <header className="header">
-            <img className="header-image" src="./images/troll-face.png" />
+            <img className="header-image" src="./images/troll-face.png" alt=""/>
             <h2 className="header-text">Meme Generator</h2>
             <h4 className="header-subText">React Project</h4>
         </header>
