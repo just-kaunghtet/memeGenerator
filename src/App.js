424407521fce98aa7d4d892fa8d34d1b903b267d
Kaung Htet <51477974+just-kaunghtet@users.noreply.github.com>
2023-12-27 08:54:56 +0630

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./components/Header"
+import Body from "./components/Body"
+/**
+ * Challenge: Build the Header component
+ */
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Body />
+        </div>
+    )
+}
